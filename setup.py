f3f377fde70dc107fb17fc9ade75145025fd3b27
ryangitsit <rmoloughlin11@gmail.com>
2023-03-28 12:17:27 -0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sim_soens',
+      version='0.1',
+      description='simulator for superconducting optoelectronic network',
+      url='https://github.com/ryangitsit/sim_soens',
+      author='Ryan O\'Loughlin',
+      author_email='rmoloughlin11@gmail.com',
+      license='NIST',
+      packages=['sim_soens'],
+      zip_safe=False)
